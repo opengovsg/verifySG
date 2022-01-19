344e2a070a92f052d8df6223ec289c95c07921b8
tanzx <tanzixiang@gmail.com>
2022-01-19 23:09:00 +0800

    refactor: refactor db-config.service.ts
    ormconfig placed in separate file and synchronize setting set dynamically based on evironment

--- backend/src/database/db-config.service.ts
+++ backend/src/database/db-config.service.ts
@@ -1,32 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm'
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
-import { join } from 'path'
 
-import { ConfigService } from 'core/providers'
+import ormConfig from './ormconfig'
 
 @Injectable()
 export class DatabaseConfigService implements TypeOrmOptionsFactory {
-  constructor(private config: ConfigService) {}
-
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: this.config.get('database.host'),
-      port: this.config.get('database.port'),
-      username: this.config.get('database.username'),
-      password: this.config.get('database.password'),
-      database: this.config.get('database.name'),
-      logging: this.config.get('database.logging'),
-      // https://docs.nestjs.com/techniques/database#auto-load-entities
-      entities: [join(__dirname, 'entities', '*.entity.js')],
-      synchronize: true, // TODO: remove in prod
-      // ref: https://github.com/typeorm/typeorm/issues/3388 to set pool size
-      extra: {
-        min: this.config.get('database.minPool'),
-        max: this.config.get('database.maxPool'),
-      },
-      namingStrategy: new SnakeNamingStrategy(),
-    }
+    return ormConfig
   }
 }
