220a83a4c89f7daf94e43638fec912fe2c85ca92
Alexis <gweiying@gmail.com>
2022-03-14 14:29:13 +0800

    feat(auth): use agencyService to get officer's agency when creating new Officer

--- backend/src/officers/officers.module.ts
+++ backend/src/officers/officers.module.ts
@@ -3,9 +3,10 @@ import { OfficersService } from './officers.service'
 import { OfficersController } from './officers.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Officer } from '../database/entities'
+import { AgenciesModule } from 'agencies/agencies.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Officer])],
+  imports: [TypeOrmModule.forFeature([Officer]), AgenciesModule],
   providers: [OfficersService],
   controllers: [OfficersController],
   exports: [OfficersService],
--- backend/src/officers/officers.service.ts
+++ backend/src/officers/officers.service.ts
@@ -1,34 +1,42 @@
 import { Injectable } from '@nestjs/common'
-import { Connection, Repository } from 'typeorm'
+import { Repository } from 'typeorm'
 import { Officer } from '../database/entities'
 import { InjectRepository } from '@nestjs/typeorm'
 import { OfficerDto, UpdateOfficerDto } from './dto/officer.dto'
+
+import { AgenciesService } from 'agencies/agencies.service'
 
 @Injectable()
 export class OfficersService {
   constructor(
     @InjectRepository(Officer) private officerRepository: Repository<Officer>,
-    private connection: Connection,
+    private agencyService: AgenciesService,
   ) {}
 
   async findOrInsert(officer: OfficerDto): Promise<Officer> {
-    return this.connection.transaction(async (manager) => {
-      const foundOfficer = await manager.getRepository(Officer).findOne({
-        where: {
-          email: officer.email,
-        },
-      })
+    const { email } = officer
+    const foundOfficer = await this.findByEmail(email)
 
-      if (foundOfficer) {
-        return foundOfficer
-      }
-      const officerToAdd = this.officerRepository.create(officer)
-      return manager.save(officerToAdd)
-    })
+    if (foundOfficer) return foundOfficer
+    return await this.createOfficer(officer)
+  }
+
+  async createOfficer(officer: OfficerDto): Promise<Officer> {
+    const agency = await this.agencyService.findByEmail(officer.email)
+    if (!agency) throw new Error(`No agency for ${officer.email} found`)
+
+    const officerToAdd = this.officerRepository.create({ ...officer, agency })
+    return this.officerRepository.save(officerToAdd)
   }
 
   async getById(id: number): Promise<Officer | undefined> {
     return this.officerRepository.findOne(id)
+  }
+
+  async findByEmail(email: string): Promise<Officer | undefined> {
+    return this.officerRepository.findOne({
+      where: { email },
+    })
   }
 
   async updateDetails(
