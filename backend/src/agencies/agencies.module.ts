5da3970a110ba49d56af2e62578e2acde5a80b00
Alexis <gweiying@gmail.com>
2022-03-14 18:06:06 +0800

    feat(auth-admin): apply auth-admin guard

--- backend/src/agencies/agencies.module.ts
+++ backend/src/agencies/agencies.module.ts
@@ -5,9 +5,10 @@ import { AgenciesService } from './agencies.service'
 import { AgenciesController } from './agencies.controller'
 
 import { Agency } from 'database/entities'
+import { AuthAdminModule } from 'auth-admin/auth-admin.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Agency])],
+  imports: [TypeOrmModule.forFeature([Agency]), AuthAdminModule],
   controllers: [AgenciesController],
   providers: [AgenciesService],
   exports: [AgenciesService],
