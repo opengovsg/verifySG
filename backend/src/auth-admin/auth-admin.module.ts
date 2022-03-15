68f807e8ff2457f11c215160f2e2487d5d58e3c1
Alexis <gweiying@gmail.com>
2022-03-15 18:27:26 +0800

    feat: update authAdminService to use keyHashService for comparing key-hash

--- backend/src/auth-admin/auth-admin.module.ts
+++ backend/src/auth-admin/auth-admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { KeyHashService } from 'common/providers/key-hash.service'
 
 import { AuthAdminService } from './auth-admin.service'
 
 @Module({
-  providers: [AuthAdminService],
+  providers: [AuthAdminService, KeyHashService],
   exports: [AuthAdminService],
 })
 export class AuthAdminModule {}
--- backend/src/auth-admin/auth-admin.service.ts
+++ backend/src/auth-admin/auth-admin.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common'
-import bcrypt from 'bcrypt'
 
 import { ConfigService } from 'core/providers'
+import { KeyHashService } from 'common/providers/key-hash.service'
 
 @Injectable()
 export class AuthAdminService {
-  constructor(private config: ConfigService) {}
+  constructor(
+    private config: ConfigService,
+    private keyHashService: KeyHashService,
+  ) {}
 
   async validateAdminKey(adminKeyHeader?: string): Promise<boolean> {
     if (!adminKeyHeader) return false
@@ -14,7 +17,7 @@ export class AuthAdminService {
     if (headerKey.toLowerCase() !== 'bearer' || !key) return false
 
     const { hash } = this.config.get('adminKey')
-    const valid = await bcrypt.compare(key, hash)
+    const valid = this.keyHashService.compareKeyHash(key, hash)
     return valid
   }
 }
