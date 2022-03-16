e652811597adf8d69fbbcab66feb24862a4f80a2
Eugene Long <orbitalsqwib@gmail.com>
2022-03-16 15:42:28 +0800

    feat: implement pin input for otp form
    - replaces regular input component on otp form

--- frontend-govt/src/components/OTPForm/OTPForm.tsx
+++ frontend-govt/src/components/OTPForm/OTPForm.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { FormControl, VStack, Text, HStack } from '@chakra-ui/react'
 import {
   Button,
   FormLabel,
   FormErrorMessage,
-  Input,
 } from '@opengovsg/design-system-react'
 import { useForm } from 'react-hook-form'
 import { useAuth } from '../../contexts/auth/AuthContext'
+import { OTPInput } from './OTPInput'
 
 interface OTPFormProps {
   email: string
@@ -24,6 +24,10 @@ const RESEND_WAIT_TIME = 30000 // 30 seconds
 export const OTPForm: React.FC<OTPFormProps> = ({ email, onSuccess }) => {
   const [canResend, setCanResend] = useState(false)
   const [resendTimer, setResendTimer] = useState(RESEND_WAIT_TIME / 1000)
+  const [isEditing, setIsEditing] = useState(false)
+
+  // specify OTP input ref for resetting focus on field reset
+  const otpInputRef = useRef<HTMLInputElement>(null)
 
   // import auth context
   const { authState, setAuthState } = useAuth()
@@ -54,10 +58,18 @@ export const OTPForm: React.FC<OTPFormProps> = ({ email, onSuccess }) => {
 
   // react-hook-form controllers
   const {
+    setValue,
+    getValues,
+    resetField,
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<OTPFormData>()
+
+  // register token field on mount
+  useEffect(() => {
+    register('token', { required: true, pattern: /\d{6}/ })
+  }, [])
 
   // handle OTP resending
   const resendOTP = () => {
@@ -69,40 +81,55 @@ export const OTPForm: React.FC<OTPFormProps> = ({ email, onSuccess }) => {
   const onSubmit = (data: OTPFormData) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { token } = data
-    //TODO: use token in auth login flow
+    //TODO: do otp validation
 
     // trigger onSuccess for now, instant login
     setAuthState({ ...authState, isAuthenticated: true, email: email })
     onSuccess()
+
+    // clear form and reset focus
+    resetField('token', { defaultValue: '' })
+    otpInputRef.current?.focus()
   }
+  const onSubmitInvalid = () => setIsEditing(false)
+  const triggerSubmit = handleSubmit(onSubmit, onSubmitInvalid)
+
+  // otp input handlers
+  const handleChange = (token: string) => {
+    setValue('token', token, { shouldValidate: true })
+  }
+  const handleBlur = (token: string) => {
+    // if token is empty, user is likely still editing the otp
+    if (token !== '') setIsEditing(false)
+  }
+  const handleFocus = () => setIsEditing(true)
 
   // error handler stubs
-  //TODO: implement error handling for auth service, email validation
+  //TODO: implement error handling for auth service, otp validation
   const hasError = () => (errors.token ? true : false)
+  const showError = isEditing ? false : hasError()
   const getErrorMessage = (): string => {
-    return 'Please provide a valid email address.'
+    return 'Please enter the 6-digit OTP in full'
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={triggerSubmit}>
       <VStack spacing={8} align="stretch">
         <FormControl id="token" isInvalid={hasError()}>
           <FormLabel isRequired>One time password</FormLabel>
           <Text color="neutral.700" mb={3}>
             Please enter the OTP sent to <strong>{email}</strong>
           </Text>
-          <Input
-            h="48px"
-            type="text"
-            inputMode="numeric"
-            pattern="\d{6}"
-            {...register('token', { required: true })}
-            autoComplete="one-time-code"
-            placeholder="e.g. 111111"
+          <OTPInput
+            ref={otpInputRef}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onComplete={() => triggerSubmit()}
+            isInvalid={showError}
+            value={getValues('token')}
           />
-          {hasError() && (
-            <FormErrorMessage>{getErrorMessage()}</FormErrorMessage>
-          )}
+          {showError && <FormErrorMessage children={getErrorMessage()} />}
         </FormControl>
         <HStack justifyContent="flex-start" spacing={6}>
           <Button size="lg" colorScheme="primary" type="submit">
