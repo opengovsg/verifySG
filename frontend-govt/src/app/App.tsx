4ac4eca4ab3066e7a33f630679c7d9501ba5b700
Alexis <gweiying@gmail.com>
2022-03-21 17:10:22 +0800

    chore: initialize queryClient for App

--- frontend-govt/src/app/App.tsx
+++ frontend-govt/src/app/App.tsx
@@ -2,19 +2,26 @@
 import '@opengovsg/design-system-react/build/fonts/inter.css'
 
 // app imports
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ThemeProvider } from '@opengovsg/design-system-react'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './AppRouter'
 import AuthProvider from '../contexts/auth/AuthProvider'
 
 export const App: React.FC = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { retry: false } },
+  })
+
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <AppRouter />
-        </AuthProvider>
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AuthProvider>
+            <AppRouter />
+          </AuthProvider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
