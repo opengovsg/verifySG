513b63c7d024e941e53886f8cda4f5368272fd14
Alexis <gweiying@gmail.com>
2022-03-21 17:02:52 +0800

    chore: split AuthState into separate isAuthenticated and userState states

--- frontend-govt/src/contexts/auth/AuthProvider.tsx
+++ frontend-govt/src/contexts/auth/AuthProvider.tsx
@@ -1,5 +1,7 @@
-import { ReactChild, ReactChildren, useState } from 'react'
-import { AuthContext, AuthState } from './AuthContext'
+import { ReactChild, ReactChildren, useState, useEffect } from 'react'
+import { AuthContext } from './AuthContext'
+
+import { AuthService } from '../../services/AuthService'
 
 // auth provider props & declaration
 interface AuthProps {
@@ -8,14 +10,41 @@ interface AuthProps {
 
 // returns a
 export const AuthProvider = ({ children }: AuthProps) => {
-  const [authState, setAuthState] = useState<AuthState>({
-    isAuthenticated: false,
-    email: '',
-    name: '',
-    _id: 1,
-  })
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | undefined>(
+    false,
+  )
+  const [officer, setOfficer] = useState<string>('')
+
+  const getOfficer = async (): Promise<void> => {
+    try {
+      const retrievedOfficer = await AuthService.whoAmI()
+      setIsAuthenticated(!!retrievedOfficer)
+      if (retrievedOfficer) setOfficer(retrievedOfficer.email)
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+
+  useEffect(() => {
+    getOfficer()
+  }, [getOfficer])
+
+  const logout = async (): Promise<void> => {
+    await AuthService.logout()
+    setIsAuthenticated(false)
+    setOfficer('')
+  }
+
   return (
-    <AuthContext.Provider value={{ authState, setAuthState }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        officer,
+        getOfficer,
+        logout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   )
