fa24c6ba046f9c0e25c22c322e441d9a17fb28c4
orbitalsqwib <21305518+orbitalsqwib@users.noreply.github.com>
2022-03-22 02:33:07 -0700

    fix: add custom mocked providers to get tests to pass first (#186)
    * fix: add custom mocked providers to get tests to pass first
    
    * fix: add cross-env to dep list to fix build

--- backend/src/mops/mops.service.spec.ts
+++ backend/src/mops/mops.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Mop } from 'database/entities'
+import { Connection } from 'typeorm'
 import { MopsService } from './mops.service'
 
 describe('MopsService', () => {
@@ -6,7 +9,21 @@ describe('MopsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MopsService],
+      providers: [
+        MopsService,
+        {
+          provide: getRepositoryToken(Mop),
+          useValue: {
+            /*TODO: ADD MOCK VALUES HERE*/
+          },
+        },
+        {
+          provide: Connection,
+          useValue: {
+            /*TODO: ADD MOCK VALUES HERE*/
+          },
+        },
+      ],
     }).compile()
 
     service = module.get<MopsService>(MopsService)
