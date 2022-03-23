16b0f1d8d28118781782c7c35fbb75f9ecbfcc57
Alexis <gweiying@gmail.com>
2022-03-23 17:18:09 +0800

    feat: add isNric() custom validation decorator

--- backend/src/calls/dto/create-call.dto.ts
+++ backend/src/calls/dto/create-call.dto.ts
@@ -1,8 +1,10 @@
 import { IsString, IsNotEmpty, IsOptional } from 'class-validator'
+import { IsNric } from 'common/decorators'
 
 export class CreateCallDto {
   @IsString()
   @IsNotEmpty()
+  @IsNric()
   nric!: string
 
   @IsString()
