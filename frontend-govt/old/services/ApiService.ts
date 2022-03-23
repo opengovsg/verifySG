846e2f185fb525e39e697d2701e65dcc0b9ea115
zx <67887489+tan-zx@users.noreply.github.com>
2022-03-23 23:30:34 +0800

    refactor: remove frontend-govt/old folder that is no longer used

--- frontend-govt/old/services/ApiService.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export const ApiService = axios.create({
-  baseURL: process.env.BACKEND_BASE_URL ?? 'http://localhost:8080/api',
-  withCredentials: true,
-})
--- frontend-govt/old/services/SocketService.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from 'dgram'
-import io from 'socket.io-client'
-
-export const socket = io(
-  process.env.BACKEND_BASE_URL ?? 'http://localhost:8080/',
-  {
-    reconnectionDelay: 10000,
-  },
-)
