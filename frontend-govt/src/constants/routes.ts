ed1cf2838a3f984fc68547d4f621ea671a78d860
orbitalsqwib <21305518+orbitalsqwib@users.noreply.github.com>
2022-03-25 12:56:51 +0800

    feat: add welcome page on first profile creation (#199)
    - public routes redirect straight to callform if already authenticated
    - if no profile exists, it redirects again to the profile page
    - ProfileGuard redirects the user to the profile form if no profile data exists
    - implements the welcome page for the profile page and adds its assets

--- frontend-govt/src/constants/routes.ts
+++ frontend-govt/src/constants/routes.ts
@@ -1,5 +1,6 @@
 //TODO: shift dashboard route to /dashboard and populate root route
 export const ROOT_ROUTE = '/'
 export const LOGIN_ROUTE = '/login'
+export const WELCOME_ROUTE = '/welcome'
 export const CALLFORM_ROUTE = '/call'
 export const PROFILE_ROUTE = '/profile'
