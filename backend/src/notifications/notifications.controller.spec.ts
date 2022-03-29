4c66df834a783b7fc00a7cbde442852fcaf993bf
zx <67887489+tan-zx@users.noreply.github.com>
2022-03-29 21:54:35 +0800

    fix: fill in dummy providers to get tests to pass first

--- backend/src/notifications/notifications.controller.spec.ts
+++ backend/src/notifications/notifications.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { NotificationsController } from './notifications.controller'
+import { NotificationsService } from './notifications.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Agency, Notification, Officer } from '../database/entities'
+import { OfficersService } from '../officers/officers.service'
+import { AgenciesService } from '../agencies/agencies.service'
 
 describe('NotificationsController', () => {
   let controller: NotificationsController
@@ -7,6 +12,29 @@ describe('NotificationsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationsController],
+      providers: [
+        NotificationsService,
+        {
+          provide: getRepositoryToken(Notification),
+          useValue: {
+            /*TODO: ADD MOCK VALUES HERE*/
+          },
+        },
+        OfficersService,
+        {
+          provide: getRepositoryToken(Officer),
+          useValue: {
+            /*TODO: ADD MOCK VALUES HERE*/
+          },
+        },
+        AgenciesService,
+        {
+          provide: getRepositoryToken(Agency),
+          useValue: {
+            /*TODO: ADD MOCK VALUES HERE*/
+          },
+        },
+      ],
     }).compile()
 
     controller = module.get<NotificationsController>(NotificationsController)
