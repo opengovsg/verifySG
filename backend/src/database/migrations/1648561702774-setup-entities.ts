3ee786a922b04035d5214140bd9af35d4a6c2d03
zx <67887489+tan-zx@users.noreply.github.com>
2022-03-29 21:48:39 +0800

    fix: re-generated migration script

--- backend/src/database/migrations/1648561702774-setup-entities.ts
+++ backend/src/database/migrations/1648561702774-setup-entities.ts
@@ -1,11 +1,17 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class setupEntities1648197891470 implements MigrationInterface {
-  name = 'setupEntities1648197891470'
+export class setupEntities1648561702774 implements MigrationInterface {
+  name = 'setupEntities1648561702774'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "call" ("id" SERIAL NOT NULL, "call_scope" text, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "officer_id" integer, CONSTRAINT "PK_2098af0169792a34f9cfdd39c47" PRIMARY KEY ("id"))`,
+      `CREATE TYPE "public"."notification_notification_type_enum" AS ENUM('SGNOTIFY', 'WHATSAPP')`,
+    )
+    await queryRunner.query(
+      `CREATE TYPE "public"."notification_status_enum" AS ENUM('NOT_SENT', 'SENT')`,
+    )
+    await queryRunner.query(
+      `CREATE TABLE "notification" ("id" SERIAL NOT NULL, "notification_type" "public"."notification_notification_type_enum" NOT NULL, "recipient_id" character varying(9) NOT NULL, "status" "public"."notification_status_enum" NOT NULL DEFAULT 'NOT_SENT', "call_scope" text, "sg_notify_params" jsonb NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "officer_id" integer, CONSTRAINT "PK_705b6c7cdf9b2c2ff7ac7872cb7" PRIMARY KEY ("id"))`,
     )
     await queryRunner.query(
       `CREATE TABLE "officer" ("id" SERIAL NOT NULL, "email" character varying(255) NOT NULL, "name" character varying(255), "position" character varying(255), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "agency_id" character varying(255), CONSTRAINT "UQ_775b4f1f79cf3492b3ac6d024aa" UNIQUE ("email"), CONSTRAINT "PK_b9bab694da36794c5065085936c" PRIMARY KEY ("id"))`,
@@ -20,7 +26,7 @@ export class setupEntities1648197891470 implements MigrationInterface {
       `CREATE INDEX "IDX_bdf5fdcd0b49753475efec8397" ON "session" ("expired_at") `,
     )
     await queryRunner.query(
-      `ALTER TABLE "call" ADD CONSTRAINT "FK_88a9024682d864fba2c1fcc0d55" FOREIGN KEY ("officer_id") REFERENCES "officer"("id") ON DELETE CASCADE ON UPDATE CASCADE`,
+      `ALTER TABLE "notification" ADD CONSTRAINT "FK_71aad6d587dd9d17dd8b8f28730" FOREIGN KEY ("officer_id") REFERENCES "officer"("id") ON DELETE CASCADE ON UPDATE CASCADE`,
     )
     await queryRunner.query(
       `ALTER TABLE "officer" ADD CONSTRAINT "FK_310d4dd0f09ed026d82763c40ca" FOREIGN KEY ("agency_id") REFERENCES "agency"("id") ON DELETE CASCADE ON UPDATE CASCADE`,
@@ -32,7 +38,7 @@ export class setupEntities1648197891470 implements MigrationInterface {
       `ALTER TABLE "officer" DROP CONSTRAINT "FK_310d4dd0f09ed026d82763c40ca"`,
     )
     await queryRunner.query(
-      `ALTER TABLE "call" DROP CONSTRAINT "FK_88a9024682d864fba2c1fcc0d55"`,
+      `ALTER TABLE "notification" DROP CONSTRAINT "FK_71aad6d587dd9d17dd8b8f28730"`,
     )
     await queryRunner.query(
       `DROP INDEX "public"."IDX_bdf5fdcd0b49753475efec8397"`,
@@ -40,6 +46,10 @@ export class setupEntities1648197891470 implements MigrationInterface {
     await queryRunner.query(`DROP TABLE "session"`)
     await queryRunner.query(`DROP TABLE "agency"`)
     await queryRunner.query(`DROP TABLE "officer"`)
-    await queryRunner.query(`DROP TABLE "call"`)
+    await queryRunner.query(`DROP TABLE "notification"`)
+    await queryRunner.query(`DROP TYPE "public"."notification_status_enum"`)
+    await queryRunner.query(
+      `DROP TYPE "public"."notification_notification_type_enum"`,
+    )
   }
 }
