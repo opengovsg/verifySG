09963103066a36a00acabba8046785ad368e8546
Austin Woon Quan <43132101+austinwoon@users.noreply.github.com>
2022-03-31 22:48:09 +0800

    Revert "chore: launch configs"
    This reverts commit 0647bf7d248b2656d41f71b691b5cf6db38336b2.

--- infra/lib/beanstalk.stack.ts
+++ infra/lib/beanstalk.stack.ts
@@ -5,7 +5,6 @@ import { BaseStackProps } from '../infra.types'
 import * as cdk from 'aws-cdk-lib'
 import * as elasticbeanstalk from 'aws-cdk-lib/aws-elasticbeanstalk'
 import * as acm from 'aws-cdk-lib/aws-certificatemanager'
-import { getEnvironmentLaunchConfigs } from '../beanstalk.util'
 
 type BeanstalkStackProps = BaseStackProps & {
   vpc: ec2.Vpc
@@ -16,10 +15,7 @@ type BeanstalkStackProps = BaseStackProps & {
   solutionStackName?: string
   minInstances?: string
   maxInstances?: string
-  sslCert: acm.Certificate
-  beanstalkEnvironmentVars: {
-    [key: string]: string
-  }[]
+  sslCert: acm.Certificate,
 }
 
 export class BeanstalkStack extends Stack {
@@ -53,86 +49,6 @@ export class BeanstalkStack extends Stack {
       { applicationName: `${props.appNamePrefix}-application` },
     )
 
-    let optionSettings: cdk.aws_elasticbeanstalk.CfnEnvironment.OptionSettingProperty[] = [
-      {
-        namespace: 'aws:autoscaling:launchconfiguration',
-        optionName: 'IamInstanceProfile',
-        value: profileName,
-      },
-      {
-        namespace: 'aws:elasticbeanstalk:cloudwatch:logs',
-        optionName: 'StreamLogs',
-        value: 'true',
-      },
-      {
-        namespace: 'aws:elasticbeanstalk:cloudwatch:logs',
-        optionName: 'RetentionInDays',
-        value: '365',
-      },
-      {
-        namespace: 'aws:elasticbeanstalk:environment',
-        optionName: 'LoadBalancerType',
-        value: 'application',
-      },
-      {
-        namespace: 'aws:autoscaling:asg',
-        optionName: 'MinSize',
-        value: props.minInstances ?? '1',
-      },
-      {
-        namespace: 'aws:autoscaling:asg',
-        optionName: 'MaxSize',
-        value: props.maxInstances ?? '10',
-      },
-      {
-        namespace: 'aws:elbv2:listener:default',
-        optionName: 'ListenerEnabled',
-        value: 'False',
-      },
-      {
-        namespace: 'aws:elbv2:listener:443',
-        optionName: 'DefaultProcess',
-        value: 'default',
-      },
-      {
-        namespace: 'aws:elbv2:listener:443',
-        optionName: 'Protocol',
-        value: 'HTTPS',
-      },
-      {
-        namespace: 'aws:elbv2:listener:443',
-        optionName: 'SSLCertificateArns',
-        value: props.sslCert.certificateArn,
-      },
-      {
-        namespace: 'aws:elbv2:listener:443',
-        optionName: 'ListenerEnabled',
-        value: 'true',
-      },
-      {
-        namespace: 'aws:ec2:vpc',
-        optionName: 'VPCId',
-        value: props.vpc.vpcId,
-      },
-      {
-        namespace: 'aws:ec2:vpc',
-        optionName: 'ELBSubnets',
-        value: props.publicSubnetIds.join(','),
-      },
-      {
-        namespace: 'aws:ec2:vpc',
-        optionName: 'Subnets',
-        value: props.ec2SubnetIds.join(','),
-      },
-      {
-        namespace: 'aws:autoscaling:launchconfiguration',
-        optionName: 'SecurityGroups',
-        value: props.securityGroup.securityGroupId,
-      },
-    ]
-
-    optionSettings.concat(getEnvironmentLaunchConfigs(props.beanstalkEnvironmentVars))
-
     const env = new elasticbeanstalk.CfnEnvironment(
       this,
       `${props.appNamePrefix}-environment`,
@@ -143,7 +59,83 @@ export class BeanstalkStack extends Stack {
           props.solutionStackName ??
           '64bit Amazon Linux 2 v5.5.0 running Node.js 14',
         applicationName: `${props.appNamePrefix}-application`,
-        optionSettings,
+        optionSettings: [
+          {
+            namespace: 'aws:autoscaling:launchconfiguration',
+            optionName: 'IamInstanceProfile',
+            value: profileName,
+          },
+          {
+            namespace: 'aws:elasticbeanstalk:cloudwatch:logs',
+            optionName: 'StreamLogs',
+            value: 'true',
+          },
+          {
+            namespace: 'aws:elasticbeanstalk:cloudwatch:logs',
+            optionName: 'RetentionInDays',
+            value: '365',
+          },
+          {
+            namespace: 'aws:elasticbeanstalk:environment',
+            optionName: 'LoadBalancerType',
+            value: 'application',
+          },
+          {
+            namespace: 'aws:autoscaling:asg',
+            optionName: 'MinSize',
+            value: props.minInstances ?? '1',
+          },
+          {
+            namespace: 'aws:autoscaling:asg',
+            optionName: 'MaxSize',
+            value: props.maxInstances ?? '10',
+          },
+          {
+            namespace: 'aws:elbv2:listener:default',
+            optionName: 'ListenerEnabled',
+            value: 'False',
+          },
+          {
+            namespace: 'aws:elbv2:listener:443',
+            optionName: 'DefaultProcess',
+            value: 'default',
+          },
+          {
+            namespace: 'aws:elbv2:listener:443',
+            optionName: 'Protocol',
+            value: 'HTTPS',
+          },
+          {
+            namespace: 'aws:elbv2:listener:443',
+            optionName: 'SSLCertificateArns',
+            value: props.sslCert.certificateArn,
+          },
+          {
+            namespace: 'aws:elbv2:listener:443',
+            optionName: 'ListenerEnabled',
+            value: 'true',
+          },
+          {
+            namespace: 'aws:ec2:vpc',
+            optionName: 'VPCId',
+            value: props.vpc.vpcId,
+          },
+          {
+            namespace: 'aws:ec2:vpc',
+            optionName: 'ELBSubnets',
+            value: props.publicSubnetIds.join(','),
+          },
+          {
+            namespace: 'aws:ec2:vpc',
+            optionName: 'Subnets',
+            value: props.ec2SubnetIds.join(','),
+          },
+          {
+            namespace: 'aws:autoscaling:launchconfiguration',
+            optionName: 'SecurityGroups',
+            value: props.securityGroup.securityGroupId,
+          },
+        ],
       },
     )
 
