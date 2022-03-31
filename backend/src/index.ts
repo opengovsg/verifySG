2bed0f463a60b764b0a34d9a3c13796fda080004
Austin Woon Quan <43132101+austinwoon@users.noreply.github.com>
2022-03-31 18:32:10 +0800

    chore: inclue dotenv import

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-
+import 'dotenv/config'
 import { AppModule } from './app.module'
 import { ConfigService, Logger } from 'core/providers'
 
