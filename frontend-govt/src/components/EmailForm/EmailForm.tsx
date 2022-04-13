2b4b9183834a34c54e6e4651b0689ddc93e5825d
zx <67887489+tan-zx@users.noreply.github.com>
2022-04-13 18:24:57 +0800

    refactor: use react hook form validation with regex (looks nicer too)

--- frontend-govt/src/components/EmailForm/EmailForm.tsx
+++ frontend-govt/src/components/EmailForm/EmailForm.tsx
@@ -44,7 +44,7 @@ export const EmailForm: React.FC<EmailFormProps> = ({ onSubmit }) => {
   const hasError = (): boolean => !!errors.email || sendOtp.isError
   const getErrorMessage = (): string => {
     return errors && errors.email
-      ? 'Please provide a valid email address.'
+      ? 'Please provide a valid .gov.sg email address.'
       : `${sendOtp.error}`
   }
 
@@ -58,9 +58,12 @@ export const EmailForm: React.FC<EmailFormProps> = ({ onSubmit }) => {
             address
           </Text>
           <Input
-            type="email"
-            {...register('email', { required: true })}
-            placeholder="e.g. jane@open.gov.sg"
+            {...register('email', {
+              required: true,
+              pattern:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+.gov.sg$/,
+            })}
+            placeholder="e.g. benjamin_tan@spf.gov.sg"
           />
           {hasError() && (
             <FormErrorMessage>{getErrorMessage()}</FormErrorMessage>
