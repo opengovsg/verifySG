eff212b1d60c1cdd6ea521e0716e3b2ccd13e7d4
zx <67887489+tan-zx@users.noreply.github.com>
2022-05-18 10:50:19 +0800

    fix: frontend console error caused by whoami API (#312)

--- backend/src/officers/dto/get-officer.dto.ts
+++ backend/src/officers/dto/get-officer.dto.ts
@@ -6,4 +6,13 @@ export type OfficerDto = Pick<Officer, 'email'>
 
 export type GetOfficerDto = Pick<Officer, 'id' | 'name' | 'position'>
 
+// TODO: use shared types to align service types with backend API types example (2/2)
+export type OfficerWhoamiDto = OfficerWhoamiSuccessDto | OfficerWhoamiFailureDto
+
+export type OfficerWhoamiSuccessDto = OfficerDto
+
+export interface OfficerWhoamiFailureDto {
+  message: string
+}
+
 export type GetOfficerProfileDto = GetOfficerDto & { agency: GetAgencyDto }
