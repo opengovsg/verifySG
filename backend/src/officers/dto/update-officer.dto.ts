88f94a2c5fc0924291d14e087c298907e992e8fc
zx <67887489+tan-zx@users.noreply.github.com>
2022-06-09 14:37:43 +0800

    fix: add ASCII validation to update officer profile dto

--- backend/src/officers/dto/update-officer.dto.ts
+++ backend/src/officers/dto/update-officer.dto.ts
@@ -1,10 +1,12 @@
-import { IsString } from 'class-validator'
+import { IsAscii, IsString } from 'class-validator'
 
 // TODO: refactor Officer DTOs into shared types (2/3)
 export class UpdateOfficerProfileDto {
   @IsString()
+  @IsAscii()
   name: string
 
   @IsString()
+  @IsAscii()
   position: string
 }
