bb1e85ab45774bc290c4d530602b3c875d8199cc
zx <67887489+tan-zx@users.noreply.github.com>
2022-06-17 22:32:48 +0800

    style: modify frontend eslint config and make attendant changes (#363)
    * style: modify frontend eslint config and make attendant changes
    
    * style: apply prettier

--- frontend-govt/config-overrides.js
+++ frontend-govt/config-overrides.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-undef
 module.exports = function override(config, env) {
   return config
 }
