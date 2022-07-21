d47fa025e0eadab5c3630b50bf11e748d09de1d5
Si Han <sihan@open.gov.sg>
2022-07-21 14:58:09 +0800

    chore: fix configuration for strictPropertyInitialization and paths

--- frontend-govt/src/types/officer.ts
+++ frontend-govt/src/types/officer.ts
@@ -1,4 +1,4 @@
-import { AgencyDto } from '~shared/src/types/api/agency'
+import { AgencyDto } from '~shared/types'
 
 // TODO: refactor Officer DTOs into shared types (1/3)
 export type OfficerDto = {
