4c621a867773520dad06cf20435a72764ee01bcb
zx <67887489+tan-zx@users.noreply.github.com>
2022-07-27 18:00:09 +0800

    feat(animation): remove abrupt shift in first login (#397)

--- frontend-govt/src/components/ProfileGuard/ProfileGuard.tsx
+++ frontend-govt/src/components/ProfileGuard/ProfileGuard.tsx
@@ -1,18 +1,21 @@
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 import { useHistory } from 'react-router-dom'
+import { Skeleton } from '@chakra-ui/react'
 
 import { WELCOME_ROUTE } from '../../constants/routes'
 import { OfficerService } from '../../services/OfficerService'
 
 export const ProfileGuard: React.FC = ({ children }) => {
+  const [hasLoaded, setHasLoaded] = useState(false)
   const history = useHistory()
 
   useQuery('profile', OfficerService.getOfficer, {
     onSuccess: ({ name, position }) => {
       if (!name || !position) history.push(WELCOME_ROUTE)
+      setHasLoaded(true)
     },
   })
-
-  // always render children
-  return <>{children}</>
+  // only render children if profile is loaded
+  return <Skeleton isLoaded={hasLoaded}>{children}</Skeleton>
 }
