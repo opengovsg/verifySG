54d63330e326e15134cee833660c2a90b108df99
Si Han <sihan@open.gov.sg>
2022-07-28 15:10:25 +0800

    refactor: remove useState and update naming

--- frontend-govt/src/components/Banner/StagingBanner.tsx
+++ frontend-govt/src/components/Banner/StagingBanner.tsx
@@ -5,12 +5,12 @@ import { EnvService } from '../../services/EnvService'
 
 import Banner from '.'
 
-const ProductionUrl = 'https://checkwho.gov.sg/login'
+const productionUrl = 'https://checkwho.gov.sg/login'
 const StagingBannerMessage = () => (
   <span>
     Staging CheckWho can only send notifications to specially provisioned
     Singpass accounts. To notify live Singpass accounts, click{' '}
-    <a href={ProductionUrl}>
+    <a href={productionUrl}>
       <u>here</u>
     </a>
     .
@@ -18,15 +18,11 @@ const StagingBannerMessage = () => (
 )
 
 export const StagingBanner: React.FC = (): JSX.Element | null => {
-  const [env, setEnv] = useState<string>('')
-
-  // call env once as env will not change in the same deployment
-  useQuery('env', EnvService.getEnv, {
+  const { data: envDto } = useQuery('env', EnvService.getEnv, {
     cacheTime: Infinity,
-    onSuccess: ({ env }) => {
-      setEnv(env)
-    },
   })
 
-  return env === 'staging' ? <Banner message={StagingBannerMessage} /> : null
+  return envDto?.env === 'staging' ? (
+    <Banner message={StagingBannerMessage} />
+  ) : null
 }
