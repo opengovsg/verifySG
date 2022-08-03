78b3787399189f8ec7ef5746e1026d45364de0d9
zx <67887489+tan-zx@users.noreply.github.com>
2022-08-03 15:24:22 +0800

    refactor: make res and req for remaining DTOs

--- backend/src/agencies/agencies.service.ts
+++ backend/src/agencies/agencies.service.ts
@@ -4,7 +4,7 @@ import { In, Repository } from 'typeorm'
 
 import { AgencyResDto } from '~shared/types/api'
 import { normalizeEmail, parseEmailDomain } from '~shared/utils/email'
-import { CreateAgencyDto, UpdateAgencyDto } from './dto'
+import { CreateAgencyReqDto, UpdateAgencyReqDto } from './dto'
 import { Agency } from 'database/entities/agency.entity'
 
 @Injectable()
@@ -13,7 +13,7 @@ export class AgenciesService {
     @InjectRepository(Agency) private agencyRepository: Repository<Agency>,
   ) {}
 
-  async createAgency(createAgencyDto: CreateAgencyDto): Promise<Agency> {
+  async createAgency(createAgencyDto: CreateAgencyReqDto): Promise<Agency> {
     const { id: agencyId } = createAgencyDto
     await this.agencyRepository.insert(createAgencyDto)
     const agency = await this.findById(agencyId)
@@ -44,7 +44,7 @@ export class AgenciesService {
 
   async updateAgency(
     agencyId: string,
-    updateAgencyDto: UpdateAgencyDto,
+    updateAgencyDto: UpdateAgencyReqDto,
   ): Promise<Agency> {
     const agencyToUpdate = await this.findById(agencyId)
     if (!agencyToUpdate) {
