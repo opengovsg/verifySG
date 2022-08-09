d61139e2f7cacf9f5a447ab29d31c98899ea511f
zx <67887489+tan-zx@users.noreply.github.com>
2022-08-09 11:12:49 +0800

    feat: create message-template.entity.ts and migration + seed data

--- backend/src/database/seeds/seed.ts
+++ backend/src/database/seeds/seed.ts
@@ -2,9 +2,10 @@
 import { DataSource } from 'typeorm'
 
 import { connectionConfig } from '../datasource'
-import { Agency } from '../entities'
+import { Agency, MessageTemplate } from '../entities'
 
 import { agenciesData } from './agenciesData'
+import { messageTemplatesData } from './messageTemplatesData'
 
 const createAgencies = async (dataSource: DataSource) => {
   dataSource
@@ -12,15 +13,42 @@ const createAgencies = async (dataSource: DataSource) => {
     .then(async () => {
       console.log('Creating agencies...')
       for (const agencyData of agenciesData) {
-        const agency = new Agency()
-        Object.assign(agency, agencyData)
+        const agency = Object.assign(new Agency(), agencyData)
         await dataSource.manager.save(agency)
       }
     })
     .then(() => {
       console.log('Agencies created successfully!')
     })
-    .catch((error) => console.log(`Something went wrong!\n${error}`))
+    .catch((error) =>
+      console.log(`Something went wrong while creating agencies!\n${error}`),
+    )
+}
+
+const loadMessageTemplates = async (dataSource: DataSource) => {
+  dataSource
+    .initialize()
+    .then(async () => {
+      console.log('Loading message templates...')
+      const messageTemplateRepo =
+        dataSource.manager.getRepository(MessageTemplate)
+      for (const messageTemplateData of messageTemplatesData) {
+        console.log(`Creating message template: ${messageTemplateData.key}...`)
+        const messageTemplate = messageTemplateRepo.create({
+          ...messageTemplateData,
+          agency: { id: messageTemplateData.agencyId },
+        })
+        await dataSource.manager.save(messageTemplate)
+      }
+    })
+    .then(() => {
+      console.log('Message templates loaded successfully!')
+    })
+    .catch((error) =>
+      console.log(
+        `Something went wrong while loading message templates!\n${error}`,
+      ),
+    )
 }
 
 const main = async () => {
@@ -28,6 +56,7 @@ const main = async () => {
   try {
     const dataSource = new DataSource(connectionConfig)
     await createAgencies(dataSource)
+    await loadMessageTemplates(dataSource)
   } catch (e) {
     console.error(e)
   }
