e0d32c914f53c52561354aec74912854652b9d4a
zx <67887489+tan-zx@users.noreply.github.com>
2022-08-09 11:14:09 +0800

    feat: create message-template mod, controller, svc, dto + modify notifications

---
+++ backend/src/message-templates/message-templates.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, NotFoundException } from '@nestjs/common'
+
+import { OfficerId } from '../common/decorators'
+import { OfficersService } from '../officers/officers.service'
+
+import { MessageTemplatesService } from './message-templates.service'
+
+import { MessageTemplatesResDto } from '~shared/types/api'
+
+@Controller('message-templates')
+export class MessageTemplatesController {
+  constructor(
+    private messageTemplatesService: MessageTemplatesService,
+    private officersService: OfficersService,
+  ) {}
+
+  @Get()
+  async all(@OfficerId() officerId: number): Promise<MessageTemplatesResDto> {
+    if (!officerId) {
+      throw new NotFoundException('Officer not logged in')
+    }
+    const officer = await this.officersService.findById(officerId)
+    if (!officer) {
+      // not including the actual officer id since this is displayed on frontend
+      throw new NotFoundException('No such officer found')
+    }
+    const { agency } = officer
+    return await this.messageTemplatesService.getMessageTemplatesByAgencyId(
+      agency.id,
+    )
+  }
+}
