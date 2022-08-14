551f2fc1738e57dd9e1c76f327866987df4b3313
zx <67887489+tan-zx@users.noreply.github.com>
2022-08-14 15:36:17 +0800

    test: set up tests for officers.service.spec.ts

--- backend/src/officers/__tests__/officers.service.spec.ts
+++ backend/src/officers/__tests__/officers.service.spec.ts
@@ -1,38 +1,65 @@
+import { createMock } from '@golevelup/ts-jest'
 import { Test, TestingModule } from '@nestjs/testing'
-import { getRepositoryToken } from '@nestjs/typeorm'
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 
 import { AgenciesService } from 'agencies/agencies.service'
 import { Agency, Officer } from 'database/entities'
 
+import { useTestDatabase } from '../../database/test-hooks'
 import { OfficersService } from '../officers.service'
 
 describe('OfficersService', () => {
   let service: OfficersService
+  let repository: Repository<Officer>
+  let agenciesService: AgenciesService
+  let agenciesRepository: Repository<Agency>
+  let resetDatabase: () => Promise<void>
+  let closeDatabase: () => Promise<void>
 
-  beforeEach(async () => {
+  const mockOfficer: Officer = createMock<Officer>({
+    email: 'benjamin_tan@spf.gov.sg',
+    name: 'Benjamin Tan',
+    position: 'Commissioner of Police',
+  })
+
+  const mockAgency: Agency = createMock<Agency>({
+    id: 'SPF',
+    name: 'Singapore Police Force',
+    emailDomains: ['spf.gov.sg'],
+  })
+
+  beforeAll(async () => {
+    const [opts, resetHook, closeHook] = await useTestDatabase()
+    resetDatabase = resetHook
+    closeDatabase = closeHook
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        OfficersService,
-        {
-          provide: getRepositoryToken(Officer),
-          useValue: {
-            /*TODO: ADD MOCK VALUES HERE*/
-          },
-        },
-        AgenciesService,
-        {
-          provide: getRepositoryToken(Agency),
-          useValue: {
-            /*TODO: ADD MOCK VALUES HERE*/
-          },
-        },
+      imports: [
+        TypeOrmModule.forRootAsync(opts),
+        // using both repositories for integration tests
+        TypeOrmModule.forFeature([Officer, Agency]),
       ],
+      providers: [OfficersService, AgenciesService],
     }).compile()
 
     service = module.get<OfficersService>(OfficersService)
+    repository = module.get(getRepositoryToken(Officer))
+    agenciesService = module.get<AgenciesService>(AgenciesService)
+    agenciesRepository = module.get(getRepositoryToken(Agency))
+  })
+
+  afterAll(async () => {
+    await closeDatabase()
+  })
+
+  beforeEach(async () => {
+    await resetDatabase()
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+    expect(repository).toBeDefined()
+    expect(agenciesService).toBeDefined()
+    expect(agenciesRepository).toBeDefined()
   })
 })
