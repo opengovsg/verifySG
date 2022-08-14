a94cdb77a2e9bd1e949133c3a2b501490c5a3edb
zx <67887489+tan-zx@users.noreply.github.com>
2022-08-14 20:40:07 +0800

    test: write tests for officer.service.spec.ts

--- backend/src/officers/__tests__/officers.service.spec.ts
+++ backend/src/officers/__tests__/officers.service.spec.ts
@@ -54,6 +54,7 @@ describe('OfficersService', () => {
 
   beforeEach(async () => {
     await resetDatabase()
+    await agenciesRepository.save(Object.assign(new Agency(), mockAgency))
   })
 
   it('should be defined', () => {
@@ -61,5 +62,73 @@ describe('OfficersService', () => {
     expect(repository).toBeDefined()
     expect(agenciesService).toBeDefined()
     expect(agenciesRepository).toBeDefined()
+  })
+
+  test('createOfficerByEmail happy path', async () => {
+    const newlyCreatedOfficer = await service.createOfficerByEmail(
+      mockOfficer.email,
+    )
+    expect(newlyCreatedOfficer.email).toBe(mockOfficer.email)
+    const officerFromRepo = await repository.findOne({
+      where: { email: mockOfficer.email },
+    })
+    expect(officerFromRepo).not.toBeNull()
+    // type casting is OK as null would be caught by previous expect statement
+    expect((officerFromRepo as Officer).email).toBe(newlyCreatedOfficer.email)
+  })
+
+  test('createOfficerByEmail unhappy path', async () => {
+    await agenciesRepository.remove(mockAgency)
+    await expect(
+      service.createOfficerByEmail(mockOfficer.email),
+    ).rejects.toThrow(`No agency for ${mockOfficer.email} found`)
+  })
+
+  test('findOrInsertByEmail happy path', async () => {
+    const mockCreateOfficerByEmail = jest.spyOn(service, 'createOfficerByEmail')
+    const newlyCreatedOfficer = await service.findOrInsertByEmail(
+      mockOfficer.email,
+    )
+    expect(newlyCreatedOfficer.email).toBe(mockOfficer.email)
+    const foundOfficer = await service.findOrInsertByEmail(mockOfficer.email)
+    expect(foundOfficer).toEqual(newlyCreatedOfficer)
+    // second call of findOrInsertByEmail should only find (and not create) officer
+    expect(mockCreateOfficerByEmail).toHaveBeenCalledTimes(1)
+  })
+
+  test('findById and findByEmail', async () => {
+    const officer = await service.findOrInsertByEmail(mockOfficer.email)
+    const foundById = await service.findById(officer.id)
+    const foundByEmail = await service.findByEmail(officer.email)
+    expect(foundById).toEqual(officer)
+    expect(foundById).toEqual(foundByEmail)
+    const nonexistentOfficer = await service.findByEmail('ben@open.gov.sg')
+    const nonexistentOfficer2 = await service.findById(123456)
+    expect(nonexistentOfficer).toBeNull()
+    expect(nonexistentOfficer2).toBeNull()
+  })
+
+  test('updateOfficer happy path', async () => {
+    const officer = await service.findOrInsertByEmail(mockOfficer.email)
+    const preUpdateOfficer = (await service.findById(officer.id)) as Officer
+    expect(preUpdateOfficer.name).toBeNull()
+    expect(preUpdateOfficer.position).toBeNull()
+    await service.updateOfficer(officer.id, {
+      name: mockOfficer.name,
+      position: mockOfficer.position,
+    })
+    const updatedOfficer = (await service.findById(officer.id)) as Officer
+    expect(updatedOfficer.name).toBe(mockOfficer.name)
+    expect(updatedOfficer.position).toBe(mockOfficer.position)
+  })
+
+  it('should throw officer not found', async () => {
+    const nonexistentId = 123456
+    await expect(
+      service.updateOfficer(nonexistentId, {
+        name: mockOfficer.name,
+        position: mockOfficer.position,
+      }),
+    ).rejects.toThrow(`Officer ${nonexistentId} not found`)
   })
 })
