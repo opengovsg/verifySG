e59934dd4aea2143805f56aabf6158b94ae31bea
zx <67887489+tan-zx@users.noreply.github.com>
2022-11-05 17:44:14 +0800

    test: playwright (#459)
    * test: initialize Playwright
    
    * build(ci): run end-to-end tests after deploy is complete
    
    * refactor: simplify yml files somewhat
    
    * build(ci): wait for deployment to complete before running e2e-tests
    
    * test: valid and invalid Singpass notification on MS Edge
    
    * test: use global-setup to avoid logging in again for different tests
    
    * refactor: light refactoring
    
    * docs: update readme
    
    * test: update deploy-app.yml to write gmail creds files
    
    * build: changes to package-lock.json
    
    * refactor: modularise GitHub workflows .yml code
    
    * refactor: fix typo in yml

---
+++ e2e/global-setup.ts
@@ -0,0 +1,37 @@
+import { chromium, expect, FullConfig } from '@playwright/test'
+import { gmailHelper } from './gmail-helper'
+
+const STAGING_URL = 'https://staging.checkwho.gov.sg'
+export const checkwhoUrl =
+  process.env.CHECKWHO_URL || STAGING_URL
+
+const checkwhoFromEmail = 'donotreply@mail.postman.gov.sg'
+const testEmail = process.env.TEST_EMAIL
+
+async function globalSetup(_: FullConfig) {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  // GO TO LOGIN PAGE
+  await page.goto(checkwhoUrl)
+  // check that title exists
+  await expect(page).toHaveTitle(/CheckWho/)
+  // check that staging banner exists if on staging version
+  if (checkwhoUrl === STAGING_URL) {
+    const stagingBanner = page.locator('text=Staging CheckWho')
+    await expect(stagingBanner).toBeVisible()
+  }
+
+  // LOGIN USING OTP
+  await page.locator('#email').fill(testEmail)
+  await page.getByRole('button', { name: 'Get OTP'}).click()
+  const otp = await gmailHelper.getOTP(checkwhoFromEmail, testEmail)
+  await page.locator('#otp').fill(otp)
+  await page.getByRole('button', { name: 'Log in'}).click()
+
+  // Save signed-in state to 'storageState.json'.
+  await page.context().storageState({ path: './e2e/storageState.json' });
+  await browser.close();
+}
+
+export default globalSetup;
---
+++ e2e/singpass.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect, Page } from '@playwright/test'
+
+import { checkwhoUrl } from './global-setup'
+
+const testRecipientNric = process.env.TEST_NRIC
+
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test.describe.serial('Test sending Singpass notification', () => {
+  test('Send valid notification successfully', async () => {
+    await page.goto(`${checkwhoUrl}/notification`)
+    await page.locator('[name="nric"]').fill(testRecipientNric)
+    // select message template
+    await page.locator('.css-1d8n9bt').click()
+    // select first option
+    await page.locator('#react-select-4-option-0').click()
+    // click send button and get response
+    // wrap in Promise.all to avoid race condition
+    const [response, _] = await Promise.all([
+      page.waitForResponse(`${checkwhoUrl}/api/notifications`),
+      page.getByRole('button', { name: 'Notify call recipient' }).click(),
+    ])
+    expect(response.ok()).toBeTruthy()
+  })
+
+  test('Send notification to invalid NRIC fails', async () => {
+    await page.locator('[name="nric"]').fill('S1234567D')
+    // no need to select template as state is kept between clicks
+    const [response, _] = await Promise.all([
+      page.waitForResponse(`${checkwhoUrl}/api/notifications`),
+      page.getByRole('button', { name: 'Notify call recipient' }).click(),
+    ])
+    expect(response.ok()).toBeFalsy()
+  })
+})
