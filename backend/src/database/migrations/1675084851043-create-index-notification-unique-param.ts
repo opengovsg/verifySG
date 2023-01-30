2c94678d439a4e734a752bea4174811392d0f3b6
zx <67887489+tan-zx@users.noreply.github.com>
2023-01-30 21:26:01 +0800

    feat: create db index for quicker queries

---
+++ backend/src/database/migrations/1675084851043-create-index-notification-unique-param.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class createIndexNotificationUniqueParam1675084851043
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE INDEX idx_notification_unique_param ON notification (SUBSTRING(modality_params ->> 'message', POSITION('check-sms-' IN modality_params ->> 'message') + 10, 12))
+WHERE
+	SUBSTRING(modality_params ->> 'message', POSITION('check-sms-' IN modality_params ->> 'message') + 10, 12) IS NOT NULL;`,
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX idx_notification_unique_param ON notification;`,
+    )
+  }
+}
